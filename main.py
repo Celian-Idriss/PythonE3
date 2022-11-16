1785b8c95349983cc41e2e2b0cbee6c340703c6b
IdrissB77 <59149839+IdrissB77@users.noreply.github.com>
2022-11-16 15:16:55 +0100

    merge

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
 import pandas as pd
 import plotly.express as px
-import numpy as np
 
-#Fonction qui prends un dataFrame en parametre et retourne le csv correspondant
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
 def dataFrame_to_csv(file):
     file.to_csv("new.csv", sep=';', index = False)
 
 if __name__ == '__main__':
+
+    app = dash.Dash(__name__)
+
     file = pd.read_xml("standard_rating_list.xml")
     #créé un histogramme avec plotly express
     fig = px.histogram(file, x="rating", nbins=18)
-    fig.show()
+
+    app.title = 'Chess Dashboard'
+    app.layout = html.Div(
+    children=[
+        html.H1(children=f'Chess Dashboard',
+            style={'textAlign': 'center', 'color': '#7FDBFF'}),
+        dcc.Graph(
+            id='graph1',
+            figure=fig
+        ),
+    ]
+    )
+
+    app.run_server(debug=True) # (8)
