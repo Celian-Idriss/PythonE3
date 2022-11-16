6fc45393d424923d9191685820a251abab24ceb1
IdrissB77 <59149839+IdrissB77@users.noreply.github.com>
2022-11-16 14:52:29 +0100

    DashBoard

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import pandas as pd
-import csv
+import plotly.express as px
+import numpy as np
 
-#Le main
 if __name__ == '__main__':
     file = pd.read_xml("standard_rating_list.xml")
-    file.to_csv("new.csv")
+    #créé un histogramme avec plotly express
+    fig = px.histogram(file, x="rating", nbins=18)
+    fig.show()
