e8255a288efadf8fd3674045131fad3e9a0da924
IdrissB77 <59149839+IdrissB77@users.noreply.github.com>
2022-12-09 13:23:18 +0100

    corrections

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import plotly.express as px
 import plotly.figure_factory as ff
 
 import dash
-from dash import dcc
+from dash import dcc, Input, Output, html
 from dash import html
 
 def dataFrame_to_csv(file):
@@ -14,27 +14,12 @@ if __name__ == '__main__':
     app = dash.Dash(__name__)
 
     file = pd.read_csv("test.csv", sep=';')
-    dataFrame_to_csv(file)
     #créé un nouvelle collone sum avec pour valeur 1 (permet de créé un compteur)
     file['sum'] = 1
 
     #créé un histogramme avec plotly express
     fig = px.histogram(file, x="rating", nbins=20)
     country_data = px.data.gapminder()
-
-
-    #Objet pour la carte 
-    carte = file.groupby("country   ")
-    print(carte.head())
-
-    #Création de la care
-    plan = px.choropleth(
-            file,
-            locations='country',
-            color="sum",
-            color_continuous_scale = px.colors.sequential.Plasma,
-            hover_name="country"
-    )
 
     #Création d'un dataframe avec deux colonnes (contry et nbr) qui pour chaque pays compte le nombre de joueurs qu'il y a
     new_file = file.groupby(['country']).sum().reset_index() 
@@ -50,13 +35,15 @@ if __name__ == '__main__':
 
 
     #créé un tableau
+    #supprime les ligne dont la valeur de la colonne flag n'est pas null
+    file_sample = file[file['flag'].isnull()]
     #supprime les colones inutile
-    file_sample = file.drop(['sum','fideid','w_title','o_title','foa_title','games','k','flag'], axis=1)
 
+    file_sample = file_sample.drop(['sum','fideid','w_title','o_title','foa_title','games','k','flag'], axis=1)
     #tri les valeurs par rating de manière décroissante
     file_sample = file_sample.sort_values(by=['rating'], ascending=False)
-    file_sample = file_sample[0:25]
-    fig2 =  ff.create_table(file_sample) 
+    file_sample = file_sample[0:10]
+    fig2 = ff.create_table(file_sample)
 
     #créé un camembert avec la somme des sexes
     fig3 = px.pie(file, values='sum', title='', names='sex')
@@ -64,35 +51,35 @@ if __name__ == '__main__':
     #supprime les valeurs null de la collone title
     fileForTitle = file[file['title'].notna()]
     #créé un camembert avec la somme des titres mais exclu les valeurs null
-    fig4 = px.pie(fileForTitle, values='iso_num', title='', names='title')
+    fig4 = px.pie(fileForTitle, values='sum', title='', names='title')
 
     app.title = 'Chess Dashboard'
     app.layout = html.Div(
-
-        children=[
-            html.H1(children=f'Chess Dashboard',
-                style={'textAlign': 'center', 'color': '#7FDBFF'}),
-            dcc.Graph(
-                id='graph1',
-                figure=fig
-            ),
-            dcc.Graph(
-                id='plan',
-                figure=plan
-            ),
-            dcc.Graph(
-                id='graph2',
-                figure=fig2
-            ),
-            dcc.Graph(
-                id='graph3',
-                figure=fig3
-            ),
-            dcc.Graph(
-                id='graph4',
-                figure=fig4
-            )
-        ]
-    )
+    children=[
+        html.H1(children=f'Chess Dashboard',
+            style={'textAlign': 'center', 'color': '#7FDBFF'}),
+        dcc.Graph(
+            id='graph1',
+            figure=fig
+        ),
+        dcc.Graph(
+            id='plan',
+            figure=plan
+        ),
+        dcc.Graph(
+            id='graph2',
+            figure=fig2
+        ),
+        dcc.Graph(
+            id='graph3',
+            figure=fig3
+        ),
+        dcc.Graph(
+            id='graph4',
+            figure=fig4
+        )
+    ]
+)
+    
 
     app.run_server(debug=True) # (8)
