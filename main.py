979c84b6d8bef4f41f3e4c9f31dad609c04f6b0a
IdrissB77 <59149839+IdrissB77@users.noreply.github.com>
2022-12-19 19:37:34 +0100

    ménage + default value

--- main.py
+++ main.py
@@ -11,20 +11,7 @@ import dash
 from dash import dcc, Input, Output, html
 from dash import html
 
-
 global file
-
-
-class MyHTMLParser(HTMLParser):
-
-    def __init__(self):
-        super().__init__()
-    def handle_starttag(self, tag, attrs):
-        print("<< Trouvé balise ouvrante :", tag)
-    def handle_endtag(self, tag):
-        print(">> Trouvé balise fermante :", tag)
-    def handle_data(self, data):
-        if data.strip(): print("Trouvé contenu  :", data)
 
 def dataFrame_to_csv(file):
     df = pd.read_xml(file)
@@ -37,9 +24,9 @@ def download_file(url, file_name):
         response = requests.get(url)
         file.write(response.content)
 
+    
 
 if __name__ == '__main__':
-
     # Récupération de la page web
     url = "https://ratings.fide.com/download.phtml"
     page = requests.get(url)
@@ -60,121 +47,20 @@ if __name__ == '__main__':
     # Décompression du fichier .zip
     with ZipFile("myfile.zip", 'r') as zipObj:
         zipObj.extractall()
-    
 
-    
     app = dash.Dash(__name__)
 
-    file = pd.read_csv("test.csv", sep=';')
-    #créé un nouvelle collone sum avec pour valeur 1 (permet de créé un compteur)
-    file['sum'] = 1
-
-    #créé un histogramme avec plotly express
-    fig = px.histogram(file, x="rating", nbins=20)
-    country_data = px.data.gapminder()
-
-    #Création d'un dataframe avec deux colonnes (contry et nbr) qui pour chaque pays compte le nombre de joueurs qu'il y a
-    new_file = file.groupby(['country']).sum().reset_index() 
-
-
-    plan = px.choropleth(
-        new_file, 
-        locations='country', 
-        color='sum',
-        labels={'sum':'number of players'},
-        projection='orthographic',
-        title="Players Repartition",
-    )
-
-
-    #créé un tableau
-    #supprime les ligne dont la valeur de la colonne flag n'est pas null
-    file_sample = file[file['flag'].isnull()]
-    #supprime les colones inutile
-
-    file_sample = file_sample.drop(['sum','fideid','w_title','o_title','foa_title','games','k','flag'], axis=1)
-    #tri les valeurs par rating de manière décroissante
-    file_sample = file_sample.sort_values(by=['rating'], ascending=False)
-    file_sample = file_sample[0:10]
-    fig2 = ff.create_table(file_sample)
-
-    #créé un camembert avec la somme des sexes
-    fig3 = px.pie(file, values='sum', title='', names='sex')
-
-    #supprime les valeurs null de la collone title
-    fileForTitle = file[file['title'].notna()]
-    #créé un camembert avec la somme des titres mais exclu les valeurs null
-    fig4 = px.pie(fileForTitle, values='sum', title='', names='title')
-
-    fileBirtday = file[file['birthday'].notna()].sort_values(by=['birthday'], ascending=False)
-
-    #créé les options pour les différents dopdown
-    optionCountry = [{'label': i, 'value': i} for i in file['country'].unique()]
-    optionSex = [{'label': i, 'value': i} for i in file['sex'].unique()]
-    optionsTitle = [{'label': i, 'value': i} for i in fileForTitle['title'].unique()]
-    optionsAge = [{'label': i, 'value': i} for i in fileBirtday['birthday'].unique()]
-
-    app.title = 'Chess Dashboard'
-    app.layout = html.Div(
-    children=[
-        html.H1(children=f'Chess Dashboard',
-            style={'textAlign': 'center', 'color': '#7FDBFF'}),
-        dcc.Graph(
-            id='graph1',
-            figure=fig
-        ),
-        dcc.Graph(
-            id='plan',
-            figure=plan
-        ),
-        #permet de rentrer un nom
-        dcc.Input(id='inputName', type='text'),
-        dcc.Dropdown(
-            id='countryDropdown',
-            options= optionCountry,
-            value='USA'
-        ),
-        dcc.Dropdown(
-            id='sexDropdown',
-            options= optionSex,
-            value=''
-        ),
-        dcc.Dropdown(
-            id='titleDropdown',
-            options= optionsTitle,
-            value=''
-        ),
-        dcc.Dropdown(
-            id='DateDropdown',
-            options= optionsAge,
-            value=''
-        ),
-        html.Button('Enter', id='button'),
-        dcc.Graph(
-            id='graph2',
-            figure=fig2
-        ),
-        dcc.Graph(
-            id='graph3',
-            figure=fig3
-        ),
-        dcc.Graph(
-            id='graph4',
-            figure=fig4
-        )
-    ]
-)
-    #met à jour le graph1 en fonction des valeurs des dropdown
     @app.callback(
         [Output('graph1', 'figure'), Output('graph2', 'figure'), Output('graph3', 'figure'), Output('graph4', 'figure'), Output('plan', 'figure')],
         [Input('countryDropdown', 'value'), Input('sexDropdown', 'value'), Input('titleDropdown', 'value'), Input('DateDropdown', 'value')]
     )
     def update(country, sex, title, date):
         global file
-        newFile = file[file['country'] == country]
-        newFile = newFile[newFile['sex'] ==  sex]
-        newFile = newFile[newFile['title'] ==  title]
-        newFile = newFile[newFile['birthday'] ==  date]
+        newFile = file
+        if country != 'ALL': newFile = file[file['country'] == country]
+        if sex != 'ALL': newFile = newFile[newFile['sex'] ==  sex]
+        if title != 'ALL': newFile = newFile[newFile['title'] ==  title]
+        if date != 'ALL': newFile = newFile[newFile['birthday'] ==  date]
         newFile = newFile[newFile['flag'].isnull()]
 
         #HISTOGRAMME
@@ -182,7 +68,8 @@ if __name__ == '__main__':
 
         #TABLEAU
         #supprime les ligne dont la valeur de la colonne flag n'est pas null
-        tableau = newFile[0:10]
+        #trie les données par rating
+        tableau = newFile.sort_values(by=['rating'], ascending=False)[0:10]
         #supprime les colones inutile
         tableau = tableau.drop(['sum','fideid','w_title','o_title','foa_title','games','k','flag'], axis=1)
         fig2 = ff.create_table(tableau)
@@ -207,5 +94,78 @@ if __name__ == '__main__':
 
         return fig, fig2, fig3, fig4, plan
 
-    app.run_server(debug=True)
+    file = pd.read_csv("test.csv", sep=';')
+    #créé un nouvelle collone sum avec pour valeur 1 (permet de créé un compteur)
+    file['sum'] = 1
 
+    #supprime les valeurs null de la collone title
+    fileForTitle = file[file['title'].notna()]
+
+    fileBirtday = file[file['birthday'].notna()].sort_values(by=['birthday'], ascending=False)
+
+    #initialise les graphes à zéro
+    fig, fig2, fig3, fig4, plan = update('USA', 'M', 'GM', '1995')
+
+    #créé les options pour les différents dropdown
+    optionCountry = [{'label': i, 'value': i} for i in file['country'].unique()]
+    optionSex = [{'label': i, 'value': i} for i in file['sex'].unique()]
+    optionsTitle = [{'label': i, 'value': i} for i in fileForTitle['title'].unique()]
+    optionsAge = [{'label': i, 'value': i} for i in fileBirtday['birthday'].unique()]
+    #ajoute une option vide
+    optionCountry.insert(0, {'label': 'ALL', 'value': 'ALL'})
+    optionSex.insert(0, {'label': 'ALL', 'value': 'ALL'})
+    optionsTitle.insert(0, {'label': 'ALL', 'value': 'ALL'})
+    optionsAge.insert(0, {'label': 'ALL', 'value': 'ALL'})
+
+    app.title = 'Chess Dashboard'
+    app.layout = html.Div(
+    children=[
+        html.H1(children=f'Chess Dashboard',
+            style={'textAlign': 'center', 'color': '#7FDBFF'}),
+        dcc.Graph(
+            id='graph1',
+            figure=fig
+        ),
+        dcc.Graph(
+            id='plan',
+            figure=plan
+        ),
+        #permet de rentrer un nom
+        dcc.Input(id='inputName', type='text'),
+        dcc.Dropdown(
+            id='countryDropdown',
+            options= optionCountry,
+            value='ALL'
+        ),
+        dcc.Dropdown(
+            id='sexDropdown',
+            options= optionSex,
+            value='ALL'
+        ),
+        dcc.Dropdown(
+            id='titleDropdown',
+            options= optionsTitle,
+            value='ALL'
+        ),
+        dcc.Dropdown(
+            id='DateDropdown',
+            options= optionsAge,
+            value='ALL'
+        ),
+        html.Button('Enter', id='button'),
+        dcc.Graph(
+            id='graph2',
+            figure=fig2
+        ),
+        dcc.Graph(
+            id='graph3',
+            figure=fig3
+        ),
+        dcc.Graph(
+            id='graph4',
+            figure=fig4
+        )
+    ]
+)
+
+    app.run_server(debug=True)
